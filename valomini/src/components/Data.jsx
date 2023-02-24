f74ade4de7314fb868cefa7e592b87858db64175
narniaeagle <ilgar2023@hotmail.com>
2023-02-24 15:38:31 -0500

    some functionality

--- valomini/src/components/Data.jsx
+++ valomini/src/components/Data.jsx
@@ -1,7 +1,49 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 export default function Data () {
-    return (
+ 
+    //making a state to set data in √
+    //setting up a useeffect to control my components lifecycle √
+    //organize API links / url √
+    // make the darned API call, right?√
+    //set our data in state and log it√
+    // render our data √
+    //set up Guard Operator √
+
+    const [agent, setAgent] = useState({})
+
+    useEffect(() => {
+        const url = 'https://valorant-api.com/v1/agents'
+        const getAgents = async () => {
+            const response = await axios.get(url)
+            console.log(response.data)
+            setAgent(response.data)
+        }
+        getAgents()
+    },[])
+
+    console.log(agent.data)
+
+    //Returns go INSIDE of our IF ELSE
+    // Ternaries go INSIDE of our Returns
+
+
+    if(agent.data){
+    return (  
         <div>
-            <h2>data goes here</h2>
+            {agent.data.map((info) =>
+            <div>
+            <h2>{info.displayName}</h2>
+            <img src={info.fullPortrait} style={{width:"24rem",height:"24rem"}}/>
+  
+            </div>
+            )}
         </div>
-    )
-}
+    )}
+    else{
+        return(
+            <h1>Loading</h1>
+        )
+    }
+  }
